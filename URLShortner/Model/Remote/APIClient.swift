d13ccc6fe144d700f47b1ce2345b6a7f443d92aa
Soheil Malekzadeh <soheil.malekzadeh@yinzcam.com>
2018-02-25 17:48:24 -0500

    Add the API classes

---
+++ URLShortner/Model/Remote/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  URLShortner
+//
+//  Created by Soheil Malekzadeh on 2/25/18.
+//  Copyright © 2018 Soheil Malekzadeh. All rights reserved.
+//
+
+import Foundation
+
+protocol APIClient {
+    
+}
