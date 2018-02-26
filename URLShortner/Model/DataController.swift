217ab3645a3502bccd03e92f5eb4179db11f7b11
Soheil Malekzadeh <soheil.malekzadeh@yinzcam.com>
2018-02-25 20:38:42 -0500

    Add the DataController class

---
+++ URLShortner/Model/DataController.swift
@@ -0,0 +1,34 @@
+//
+//  DataController.swift
+//  URLShortner
+//
+//  Created by Soheil Malekzadeh on 2/25/18.
+//  Copyright © 2018 Soheil Malekzadeh. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    let persistentContainer: NSPersistentContainer
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(_ modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func loadPersistentStores(_ completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { (description, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            if let completion = completion {
+                completion()
+            }
+        }
+    }
+    
+}
